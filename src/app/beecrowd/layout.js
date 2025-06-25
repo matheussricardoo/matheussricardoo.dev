fcad87bd22bca6419489bacb336ef5b78d18dc4c
matheussricardoo <matheusri1@hotmail.com>
2025-06-25 20:22:09 -0300

    feat: Cria novo portfólio com design moderno e interativo

--- src/app/beecrowd/layout.js
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: 'Beecrowd Solutions | Matheus Ricardo',
-  description: 'Soluções para problemas do Beecrowd em Java e Python',
-}
-
-export default function BeecrowdLayout({ children }) {
-  return <>{children}</>
-}
--- src/app/mooc/layout.js
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: 'MOOC Helsinki | Matheus Ricardo',
-  description: 'Exercícios do curso de Java da Universidade de Helsinki',
-}
-
-export default function MoocLayout({ children }) {
-  return <>{children}</>
-}
